90a264ec48f452f3415994c6f3fb8ff3df06b2b2
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2023-04-10 18:01:07 +0300

    fix remove lint mistake

--- utils/test-utils.tsx
+++ utils/test-utils.tsx
@@ -1,24 +1,6 @@
 import { cleanup, render } from '@testing-library/react';
 import { afterEach } from 'vitest';
-const products = [
-  {
-    id: 1,
-    title: 'iPhone 9',
-    description: 'An apple mobile which is nothing like apple',
-    price: 549,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: 'Apple',
-    category: 'smartphones',
-    thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    images: [
-      'https://i.dummyjson.com/data/products/1/1.jpg',
-      'https://i.dummyjson.com/data/products/1/3.jpg',
-      'https://i.dummyjson.com/data/products/1/4.jpg',
-    ],
-  },
-];
+
 afterEach(() => {
   cleanup();
 });
